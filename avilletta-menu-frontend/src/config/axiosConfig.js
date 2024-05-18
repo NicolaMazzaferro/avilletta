a370d3711dd43ef0726f5976b21ca5c3fb9c6cf3
NicolaMazzaferro <nicolamazzaferro9@gmail.com>
2024-05-18 03:08:57 +0200

    aggiornato url API

--- avilletta-menu-frontend/src/config/axiosConfig.js
+++ avilletta-menu-frontend/src/config/axiosConfig.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost:8000/api/',
+  baseURL: 'http://menu.avillettapizzeria.it/api/',
 });
 
 export default axiosInstance;
